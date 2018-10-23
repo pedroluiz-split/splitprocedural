e8c37ef80e03de6b46b74597c77d28072f8855ab
pedroluiz-split <pedroluiz.split@gmail.com>
2018-10-23 16:07:06 -0300

    Arma e Karma implementados
    Arma e Karma já implementados. O GameObject do texto de arma já está com uma lista de armas que pode ser alterada e expandida.
    No GameObject do texto do Karma é possível alterar o período que ele gera números aleatórios, mas eu deixei setado em -100 a 100 como foi falado.

---
+++ PrototipoZumbi/Assets/Scripts/ListaStrings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListaStrings : MonoBehaviour {
+
+	public string [] objetos;
+	private string textoOriginal;
+
+	// Use this for initialization
+	void Start () {
+		textoOriginal = GetComponent<Text>().text;
+		MudarObjeto();
+	}
+
+	public void MudarObjeto ()
+	{
+		GetComponent<Text>().text = textoOriginal + objetos[Random.Range(0, objetos.Length)];
+	}
+
+}
---
+++ PrototipoZumbi/Assets/Scripts/RandomNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomNumber : MonoBehaviour {
+
+	public float min;
+	public float max;
+	private float numEscolhido;
+	private string textoInicial;
+
+	// Use this for initialization
+	void Start () {
+		textoInicial = GetComponent<Text>().text;
+		MudarNumero();
+	}
+	
+	public void MudarNumero ()
+	{
+		numEscolhido = Random.Range(min, max);
+		GetComponent<Text>().text = textoInicial + (int)numEscolhido;
+	}
+}
